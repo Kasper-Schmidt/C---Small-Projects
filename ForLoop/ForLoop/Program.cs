9c1e56a758dfe7ef45d6c6788c89da9a117090fc
Kasper Schmidt <94352891+Kasper-Schmidt@users.noreply.github.com>
2026-03-30 15:39:57 +0200

    next

---
+++ ForLoop/ForLoop/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ForLoop
+    {
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for ( int i = 0; i <= 10; i++ )
+            {
+                Console.WriteLine(i);
+            }
+
+
+            Console.WriteLine("----------------------");
+
+
+            for (int j = 0; j <= 10; j+=2)
+            {
+                Console.WriteLine(j);
+            }
+
+
+            Console.WriteLine("----------------------");
+
+            for (int k = 10; k >= 0; k--)
+            {
+                Console.WriteLine(k);
+            }
+            Console.WriteLine("Happy New Years!");
+
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
