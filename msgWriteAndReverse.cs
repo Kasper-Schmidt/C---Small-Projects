e15865f5b6b49931b6f7ad3cf20f6407e21578b0
Kasper Schmidt <94352891+Kasper-Schmidt@users.noreply.github.com>
2026-05-04 14:00:40 +0200

    Add files via upload

---
+++ msgWriteAndReverse.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace CoffeeNCode
+{
+    internal class MsgWriteAndReverse
+    {
+        public static void Run()
+        {
+            Console.Write("Enter a message: ");
+            string message = Console.ReadLine();
+
+
+
+            // Console.WriteLine(message);
+            // Console.WriteLine(message[0]);
+
+
+
+            for (int i = 0; i < message.Length; i++)
+            {
+                Console.Write(message[i]);
+            }
+
+
+
+            Console.WriteLine();
+
+
+
+            for (int i = message.Length - 1; i >= 0; i--)
+            {
+                Console.Write(message[i]);
+            }
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
